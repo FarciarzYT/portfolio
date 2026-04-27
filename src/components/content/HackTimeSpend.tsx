6e921bbda4da4d4209898d989a939feb72527cfa
FarciarzYT <140611697+FarciarzYT@users.noreply.github.com>
2026-04-27 23:09:28 +0200

    center img

--- src/components/content/HackTimeSpend.tsx
+++ src/components/content/HackTimeSpend.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const HackTimeSpend = () => {
     return (
-        <section className="mx-auto w-full max-w-3xl mt-32 hidden sm:flex">
+        <section className="mx-auto w-full max-w-3xl mt-32 hidden sm:flex justify-center items-center">
             <Link href="https://heatmap.shymike.dev?id=25343&timezone=Europe%2FWarsaw&rounding=60&labels=true&ranges=75%2C50%2C30&year=current&theme=light&standalone=true"
                   title="Click to view detailed data for each day!"
             >
