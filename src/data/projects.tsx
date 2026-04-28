5d3a68bc8b508513e783ac9e7a055af79b378024
FarciarzYT <140611697+FarciarzYT@users.noreply.github.com>
2026-04-28 21:18:10 +0200

    sort images for better organization

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -7,34 +7,34 @@ export const ProjectsList:ProjectType[] = [
         github: "https://github.com/TomaszTlusty/kiedywakacje.com",
         demo: "https://kiedywakacje.com/",
         description: "Odliczanie do wakacji, dla uczniów przez uczniów",
-        href: "/img/kiedywakacje.com.png"
+        href: "/img/projects/kiedywakacje.com.png"
     },
     {
         title: "WebSecurityLearningLabs",
         github: "https://github.com/TomaszTlusty/WebSecurityLearningLabs.git",
         demo: "https://web-security-learning-labs.vercel.app/",
         description: "Edukacyjna aplikacja internetowa do nauki cyber bezpieczeństwa dla studentów",
-        href: "/img/web-security-learning-labs.com.png"
+        href: "/img/projects/web-security-learning-labs.com.png"
     },
     {
         title: "KRC20 checker",
         github: "https://github.com/TomaszTlusty/KRC20-Checker",
         demo: "false",
         description: "Aplikacja do szukania portfeli tokenów kryptowalut Kaspa",
-        href: "/img/KRC20.png"
+        href: "/img/projects/KRC20.png"
     },
     {
         title: "Java Toolbox",
         github: "https://github.com/TomaszTlusty/JavaToolbox",
         demo: "https://github.com/TomaszTlusty/JavaToolbox/releases/tag/v1",
         description: "JavaToolbox for cybersecurity",
-        href: "/img/toolbox.png"
+        href: "/img/projects/toolbox.png"
     },
     {
         title: "Pasjans CLI",
         github: "https://github.com/TomaszTlusty/KlondikeSolitaireCLI",
         demo: "false",
         description: "konsolówka do gry w pasjansa",
-        href: "/img/pasjans.png"
+        href: "/img/projects/pasjans.png"
     }
 ]
