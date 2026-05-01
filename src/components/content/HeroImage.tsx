995de5f11711028237a1fe889b923dab93e77550
FarciarzYT <140611697+FarciarzYT@users.noreply.github.com>
2026-05-01 14:35:06 +0200

    add priority for images LCP

--- src/components/content/HeroImage.tsx
+++ src/components/content/HeroImage.tsx
@@ -8,7 +8,7 @@ export default function HeroImage({ alt }: { alt: string }) {
 
     return (
         <div
-            className="relative w-[310px] h-[370px] mx-auto sm:ml-auto sm:mr-4 cursor-pointer"
+            className="relative w-77.5 h-92.5 mx-auto sm:ml-auto sm:mr-4 cursor-pointer"
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
         >
@@ -16,6 +16,7 @@ export default function HeroImage({ alt }: { alt: string }) {
                 src="/img/tlusty.webp"
                 alt={alt}
                 fill
+                priority
                 loading="eager"
                 className={`rounded-3xl object-cover object-top transition-opacity duration-500 ${
                     hovered ? 'opacity-0' : 'opacity-100'
@@ -25,6 +26,7 @@ export default function HeroImage({ alt }: { alt: string }) {
                 src="/img/tlusty-avatar.png"
                 alt="Avatar"
                 fill
+                priority
                 className={`rounded-3xl object-cover object-top transition-opacity duration-500 ${
                     hovered ? 'opacity-100' : 'opacity-0'
                 }`}
